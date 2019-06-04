b946c6c04d1eb0b04487164d7b8a520a07d6c533
Pavol Hejný <me@pavolhejny.com>
2019-06-04 19:45:52 +0200

    Everything in dir src

---
+++ src/utils/sample-api.ts
@@ -0,0 +1,29 @@
+import { Talk } from '../model/Talk';
+
+/** Dummy user data. */
+export const dataArray: Talk[] = [new Talk({ id: 1 }), new Talk({ id: 1 })];
+
+/**
+ * Calls a mock API which finds a user by ID from the list above.
+ *
+ * Throws an error if not found.
+ */
+export async function findData(id: number | string) {
+    const selected = dataArray.find((data) => data.id === Number(id));
+
+    if (!selected) {
+        throw new Error('Cannot find user');
+    }
+
+    return selected;
+}
+
+/** Calls a mock API which returns the above array to simulate "get all". */
+export async function findAll() {
+    // Throw an error, just for example.
+    if (!Array.isArray(dataArray)) {
+        throw new Error('Cannot find users');
+    }
+
+    return dataArray;
+}
