72c77dc47cdef10807ba0d17a7b11aa9821d0d4d
Pavol Hejný <me@pavolhejny.com>
2019-06-04 18:20:09 +0200

    Sample boilerplate

---
+++ .next/static/webpack/static/development/pages/next/dist/pages/_error.js.e52e32a8e91fc41c120b.hot-update.js
@@ -0,0 +1,18 @@
+webpackHotUpdate("static\\development\\pages\\next\\dist\\pages\\_error.js",[
+/* 0 */
+false,
+/* 1 */,
+/* 2 */,
+/* 3 */
+/*!*****************************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fnext%2Fdist%2Fpages%2F_error&absolutePagePath=C%3A%5CUsers%5Chejny%5Cwork%5Chejny%5Ctalks%5Cnode_modules%5Cnext%5Cdist%5Cpages%5C_error.js ***!
+  \*****************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fnext%2Fdist%2Fpages%2F_error&absolutePagePath=C%3A%5CUsers%5Chejny%5Cwork%5Chejny%5Ctalks%5Cnode_modules%5Cnext%5Cdist%5Cpages%5C_error.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fnext%2Fdist%2Fpages%2F_error&absolutePagePath=C%3A%5CUsers%5Chejny%5Cwork%5Chejny%5Ctalks%5Cnode_modules%5Cnext%5Cdist%5Cpages%5C_error.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=_error.js.e52e32a8e91fc41c120b.hot-update.js.map
