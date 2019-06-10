0dd2cb1aa39504f510af3403f2ed1983a2c36673
Pavol Hejný <me@pavolhejny.com>
2019-06-10 03:10:39 +0200

    Not autorun the terminals

--- src/utils/sample-api.ts
+++ src/utils/sample-api.ts
@@ -22,8 +22,8 @@ export async function findData(id: number | string) {
         `https://docs.google.com/spreadsheets/d/e/2PACX-1vRILhqlYdHZYOHa8_5awq6Z5bo4Sfhr2eFMjeQ7qwqDLbfR4gt44PqjfHKApmjgNbPjbc8FlStKhKiP/pub?gid=0&single=true&output=csv`,
     );
     const dataString = await response.text();
-    const { data } = papaparse.parse(dataString,{
-        header: true
+    const { data } = papaparse.parse(dataString, {
+        header: true,
     });
 
     selected.description = JSON.stringify(data);
